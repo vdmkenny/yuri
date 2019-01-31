40e6f08cc562506d721d1379faa20a8bc5b2b519
Kenny Van de Maele <vdmkenny@inuits.eu>
2019-01-31 17:59:09 +0100

    add ddg command
    Signed-off-by: Kenny Van de Maele <vdmkenny@inuits.eu>

--- commands.py
+++ commands.py
@@ -1,21 +1,43 @@
-import re
+import re, sys
 from pprint import pprint
 
 import config
+import common
 
-def test(message):
-  pprint(config.rocket.chat_post_message('This is a reply to the !test command!', channel=config.bot['bot_config']['channel'], alias=config.bot['bot_config']['alias']).json())
+sys.path.append('duckduckgo')
+import DuckDuckGo
 
-def ping(message):
-  pprint(config.rocket.chat_post_message('Pong!', channel=config.bot['bot_config']['channel'], alias=config.bot['bot_config']['alias']).json())
+
+def test(message, param):
+  common.send_message('This is a reply to the !test command!')
+
+def ping(message, param):
+  common.send_message('Pong!')
+
+def ddg(message, param):
+  q = DuckDuckGo.search(param)
+  if q.abstract.abstract:
+    common.send_message("*" + q.abstract.heading + "*:\n" + q.abstract.abstract_text + "\n" + q.abstract.abstract_url)
+  elif q.definition.definition:
+    common.send_message("*" + param  + "*:\n" + q.definition.definition)
+  elif q.answer.answer:
+    common.send_message("*" + param  + "*:\n" + q.answer.answer)
+  elif q.results:
+    common.send_message("*" + param  + "*:\n" + q.results[0].text + "\n" + q.results[0].first_url)
+  elif q.related_topics:
+    common.send_message("*" + param  + "*:\n" + q.related_topics[0].text + "\n" + q.related_topics[0].first_url)
+  else:
+    common.send_message("I couldn't find anything for _" + param + "_!")
 
 def parse(message):
   commands = {'!test': test,
               '!ping': ping,
+              '!ddg': ddg,
   }
 
   is_command = re.match('^\!(.+)', message['messages'][0]['msg'])
   command = message['messages'][0]['msg'].partition(' ')[0]
 
   if is_command and command in commands:
-    commands[command](message)
+    param = ' '.join(message['messages'][0]['msg'].split()[1:])
+    commands[command](message, param)
--- common.py
+++ common.py
@@ -2,5 +2,9 @@ import config
 from pprint import pprint
 
 def send_message(msg = None):
-    pprint(config.rocket.chat_post_message(msg, channel=config.bot['bot_config']['channel'], alias=config.bot['bot_config']['alias']).json())
+    pprint(config.rocket.chat_post_message(msg, 
+        channel=config.bot['bot_config']['channel'], 
+        alias=config.bot['bot_config']['alias'],
+        avatar=config.bot['bot_config']['avatar']
+        ).json())
 
