1f0499739e3da603ce1920546d7ec334e54d5c42
Kenny Van de Maele <vdmkenny@inuits.eu>
2019-02-01 14:07:25 +0100

    don't log duplicate messages
    Signed-off-by: Kenny Van de Maele <vdmkenny@inuits.eu>

--- common.py
+++ common.py
@@ -2,10 +2,10 @@ import config
 from pprint import pprint
 
 def send_message(msg = None, img = None):
-    pprint(config.rocket.chat_post_message(msg, 
+    config.rocket.chat_post_message(msg, 
         channel=config.bot['bot_config']['channel'], 
         alias=config.bot['bot_config']['alias'],
         avatar=config.bot['bot_config']['avatar'],
         attachments=[{'image_url': img}]
-        ).json())
+        ).json()
 
